a73049d132aa8c16549d30520206a5bdbdc1092a
francis-ohara <francis-ohara@outlook.com>
2022-10-26 11:09:38 +0000

    Add 5: php tutorials

---
+++ 5-php/page1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head></head>
+<body>
+    <main>
+        <form action="process.php" method="POST">
+        <label><input type="text" id="username" name="username"> Username:</label>
+        <label><input type="text" id="password" name="password">Password:</label>
+        <button type="submit">Send</button>
+        </form>
+    <main>
+</body>
+</html>
