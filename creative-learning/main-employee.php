64a422413246235214d2d605c5e2c568c8544816
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-12-05 11:29:21 +0000

    update project: creative_learning

--- creative-learning/main-employee.php
+++ creative-learning/main-employee.php
@@ -1,22 +1,41 @@
-
 <h1>Welcome to the Main Employee Page!</h1>
-
+<div class="header">
+    <a href="login.php">Log Out</a>
+</div>
+<?php if (isset($_POST["upload"])) {
+    uploader();
+    update_database();
+    echo "<p><strong>Memo Uploaded Successfully!<br> Log out or Upload New Memo!</strong></p>";
+} ?>
 <form action="" method="post" enctype="multipart/form-data">
     <fieldset>
         <p>Upload Your Sales Memo Here:</p>
         <label>
 
-            <input type="file" name="memo" id="memo" accept=".docx,.doc">
+            <input type="file" name="memo" id="memo" accept=".docx,.doc" required>
         </label>
         <br><br>
-    <button type="submit" name="upload">Upload File</button>
+        <button type="submit" name="upload">Upload File</button>
     </fieldset>
 
 </form>
+
 <?php
-if (isset($_POST["upload"])){
+function uploader()
+{
     $temp = $_FILES["memo"]["tmp_name"];
     $actual = $_FILES["memo"]["name"];
-    move_uploaded_file($temp, "./memos/{$actual}.docx");
+    move_uploaded_file($temp, "./memos/{$actual}");
+    $zip = new ZipArchive();
+    $zip->open("./memos/{$actual}", ZipArchive::CREATE);
+    $xml = $zip->getFromName("word/document.xml");
+    $xml_handle = new DOMDocument();
+    $xml_handle->loadXML($xml);
+    $xml_handle->save("./xml-files/{$actual}.xml");
+
+}
+
+function update_database(){
+
 }
 ?>
--- creative-learning/main-manager.php
+++ creative-learning/main-manager.php
@@ -3,3 +3,14 @@
 <h1>Welcome to the Main Manager Page!</h1>
 <?php
 echo "You are most welcome,".$_GET["username"]."!";
+//Download xml
+echo "<a href='xml-files/Creative%20Learning%20Store%20A%20Memo%201.docx.xml' download>Download XML</a>";
+
+// Table of New Memos
+// Memo no. Memo title. Uploaded by, upload date, AlreadyDownloaded?- yes or no - buttons(Download XML, Download Word file).
+?>
+<br><br>
+
+Download word
+From employee - view employee details
+
